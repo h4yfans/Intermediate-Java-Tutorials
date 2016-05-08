bd1a9dfcdb0c4cd1ed439b87ec795883afcb05bc
Kaan Karaca <kaan94karaca@gmail.com>
2016-05-09 00:50:56 +0300

    Added files via upload

---
+++ Applets-Introduction/src/amras.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by KAAN on 09/05/2016.
+ */
+public class amras extends JApplet {
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawString("works", 25, 25);
+    }
+
+
+
+
+}
