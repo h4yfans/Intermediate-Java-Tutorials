7e14f5ce09d9a48dfd515f050aaafadb81bf798a
Kaan Karaca <kaan94karaca@gmail.com>
2016-05-08 18:08:03 +0300

    Added files via upload

---
+++ MethodsReverseandCopy/src/amras.java
@@ -0,0 +1,47 @@
+
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by KAAN on 08/05/2016.
+ */
+public class amras {
+    public static void main(String[] args) {
+
+        // create an array and convert to list
+        Character[] ray = {'p', 'w', 'n'};
+        List<Character> l = Arrays.asList(ray);
+        System.out.println("List is: ");
+        output(l);
+
+
+        // reverse and print out the list
+        Collections.reverse(l);
+        System.out.println("After reverse : ");
+        output(l);
+
+
+        //create new array a new list
+        Character[] newRay = new Character[3];
+        List<Character> listCopy = Arrays.asList(newRay);
+
+        // copy contents of list into list copy
+        Collections.copy(listCopy, l);
+        System.out.println("Copy of list : ");
+        output(listCopy);
+
+        //fill collection with crap
+        Collections.fill(l,'X');
+        System.out.println("After filling the list : ");
+        output(l);
+    }
+
+    private static void output(List<Character> thelist) {
+        for (Character thing: thelist)
+            System.out.printf("%s ",thing);
+
+        System.out.println();
+    }
+}
